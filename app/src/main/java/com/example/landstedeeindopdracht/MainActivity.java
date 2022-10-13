6d493db0fc79ea9c87dc9f99a3748a1d74d28425
Cheexey <85673462+Cheexey@users.noreply.github.com>
2022-10-13 10:06:52 +0200

    penis2

--- app/src/main/java/com/example/landstedeeindopdracht/MainActivity.java
+++ app/src/main/java/com/example/landstedeeindopdracht/MainActivity.java
@@ -31,6 +31,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -40,31 +42,44 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import javax.security.auth.login.LoginException;
+
 
 public class MainActivity extends AppCompatActivity {
+    //presets
     static final int REQUEST_IMAGE_CAPTURE = 1;
     public Bitmap Foto;
+    public static final int REQUEST_CODE_PERMISSION = 2;
 
+    //Create TakePicture Intent
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
+        String[] mPermission = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        int check = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
+        if(check != 0){
+            ActivityCompat.requestPermissions(MainActivity.this, mPermission, REQUEST_CODE_PERMISSION);
+        }
+
+        //Open camera button
         Button button = (Button)findViewById(R.id.StartFotoEvent);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //If the device does have a camera
                 if(getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
                     Log.i("camera", "Device has camera");
                     dispatchTakePictureIntent();
                 }
                 else{
+                    //If the device does not have a camera
                     Log.i("camera", "Device does not have camera");
                 }
             }
@@ -78,14 +93,24 @@ public class MainActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             Foto = imageBitmap;
-            imageView.setImageBitmap(imageBitmap);
             Intent NewActivity = new Intent(MainActivity.this, fotoVerwerken.class);
+            //Puts the picture in the new activity
             NewActivity.putExtra("BitmapImage", Foto);
+            //Start new activity
             startActivity(NewActivity);
+
+            try {
+                String baseDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+                String filename = "myFile.png";
+                File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), filename);
+                FileOutputStream out = new FileOutputStream(file); //HET GAAT HIER FOUT
+                Log.i("Success", "File is written to external storage.");
+                imageBitmap.compress(Bitmap.CompressFormat.PNG, 100,out);
+                out.flush();
+                out.close();
+            }catch (IOException e){
+                Log.i("Error", "File failed to be written to external storage " + e.getMessage());
+            }
         }
     }
 }
-
-
-
-
--- app/src/main/java/com/example/landstedeeindopdracht/fotoVerwerken.java
+++ app/src/main/java/com/example/landstedeeindopdracht/fotoVerwerken.java
@@ -4,15 +4,16 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class fotoVerwerken extends AppCompatActivity {
     public Bitmap Foto;
-
-
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
@@ -26,5 +27,15 @@ public class fotoVerwerken extends AppCompatActivity {
             Foto = (Bitmap) intent.getParcelableExtra("BitmapImage");
             Log.d("henk", "onActivityResult: henk");
             imageView.setImageBitmap(Foto);
+
+
+        Button button = (Button)findViewById(R.id.prevpage);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(fotoVerwerken.this, MainActivity.class));
+            }
+        });
     }
 }
